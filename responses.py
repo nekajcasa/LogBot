4303dd1b171f489ec3ba3a5b7886f6efa0f111a5
nekajcasa <maticko.music@gmail.com>
2023-04-24 20:45:46 +0200

    LogBot
    first test

---
+++ responses.py
@@ -0,0 +1,14 @@
+def handle_response(message):
+  p_message = message.lower()
+
+  print(f"Prejteo sporocilo: {message} => {p_message}")
+
+  if p_message == "!help":
+    return " `YOUR MESSAGE IN HERE`"
+  else:
+    return p_message
+
+
+def rezerviraj_termin(message):
+  
+  return "Označen termin !"
